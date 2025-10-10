9097a064ed41f7e8fbfaf03d7948434425ce87bc
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2025-10-11 00:45:04 +0600

    search bar

--- frontend/src/components/Common/Header.jsx
+++ frontend/src/components/Common/Header.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import TopBar from "../Layout/TopBar";
+import NavBar from "./NavBar";
 
 const Header = () => {
   return (
-    <>
+    <header>
       {/* top bar */}
       <TopBar />
       {/* nav bar */}
+      <NavBar />
       {/* cart drawer */}
-    </>
+    </header>
   );
 };
 
--- frontend/src/components/Layout/TopBar.jsx
+++ frontend/src/components/Layout/TopBar.jsx
@@ -1,25 +1,31 @@
-import {TbBrandMeta} from "react-icons/tb"
-import {IoLogoInstagram} from "react-icons/io5"
-import {RiTwitterXLine} from "react-icons/ri"
+import { TbBrandMeta } from "react-icons/tb";
+import { IoLogoInstagram } from "react-icons/io5";
+import { RiTwitterXLine } from "react-icons/ri";
 
 const TopBar = () => {
   return (
     <div className="bg-[#ea2e0e] text-white">
-        <div className="container mx-auto">
-            <div className="flex space-x-6 py-2">
-                <a href="#" className="hover:text-gray-300">
-                    <TbBrandMeta className="h-5 w-5"></TbBrandMeta>
-                </a>
-                <a href="#" className="hover:text-gray-300">
-                    <IoLogoInstagram className="h-5 w-5"></IoLogoInstagram>
-                </a>
-                <a href="#" className="hover:text-gray-300">
-                    <RiTwitterXLine className="h-5 w-5"></RiTwitterXLine>
-                </a>
-            </div>
+      <div className="container mx-auto flex justify-between items-center py-3 px-4">
+        <div className="hidden md:flex items-center space-x-4">
+          <a href="#" className="hover:text-gray-300">
+            <TbBrandMeta className="h-5 w-5"></TbBrandMeta>
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            <IoLogoInstagram className="h-5 w-5"></IoLogoInstagram>
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            <RiTwitterXLine className="h-4 w-4"></RiTwitterXLine>
+          </a>
         </div>
+        <div className="text-sm text-center">
+            <span>We ship world wide - Fast and reliable shipping</span>
+        </div>
+        <div className="text-sm hidden md:block">
+            <a href="tel: +8801677872082" className="hover:text-gray-300"> +880 167-787-2082</a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TopBar
+export default TopBar;
