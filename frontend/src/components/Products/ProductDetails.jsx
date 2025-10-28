9b5b0af1a619242b6b3b3018331031760a39b50f
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2025-10-28 17:17:47 +0600

    added mobile features

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { toast } from "sonner";
 const selectedProduct = {
   id: 1,
   name: "Jacket Product",
   price: 19.99,
-  brand:"Sailor",
+  brand: "Sailor",
   originalPrice: 29.99,
   description: "This is a sample product description.",
   sizes: ["S", "M", "L", "XL"],
@@ -22,6 +23,40 @@ const selectedProduct = {
   ],
 };
 const ProductDetails = () => {
+  const [mainImage, setMainImage] = useState("");
+  const [selectedSize, setSelectedSize] = useState("");
+  const [selectedColor, setSelectedColor] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
+  useEffect(() => {
+    if (selectedProduct?.images?.length > 0) {
+      setMainImage(selectedProduct.images[0].url);
+    }
+  }, [selectedProduct]);
+
+  const handleQuantityChange = (action) => {
+    if(action === "plus") setQuantity((prev) => prev + 1);
+    if(action === "minus" && quantity > 1) setQuantity((prev) => prev - 1);
+  };
+
+  const handleAddToCart = () => {
+    // Add to cart logic here
+    if(!selectedSize || !selectedColor){
+      toast.error("Please select size and color before adding to cart.",{
+        duration: 1000,
+      });
+      return;
+    }
+    setIsButtonDisabled(true);
+    setTimeout(() => {
+      toast.success("Product added to cart!",{
+        duration: 1000,
+      });
+      setIsButtonDisabled(false);
+    },500);
+  }
+
   return (
     <div className="p-6">
       <div className="max-w-6xl mx-auto bg-white p-8 rounded-lg">
@@ -33,7 +68,10 @@ const ProductDetails = () => {
                 key={index}
                 src={image.url}
                 alt={image.altText || `Thumbnail ${index}`}
-                className="w-20 h-20 object-cover rounded-lg cursor-pointer border"
+                className={`w-20 h-20 object-cover rounded-lg cursor-pointer border ${
+                  mainImage === image.url ? "border-black" : "border-gray-300"
+                }`}
+                onClick={() => setMainImage(image.url)}
               />
             ))}
           </div>
@@ -41,7 +79,7 @@ const ProductDetails = () => {
           <div className="md:w-1/2">
             <div className="mb-4">
               <img
-                src={selectedProduct.images[0]?.url}
+                src={mainImage}
                 alt="Main Product"
                 className="w-full h-auto object-cover rounded-lg"
               />
@@ -54,7 +92,10 @@ const ProductDetails = () => {
                 key={index}
                 src={image.url}
                 alt={image.altText || `Thumbnail ${index}`}
-                className="w-20 h-20 object-cover rounded-lg cursor-pointer border"
+                className={`w-20 h-20 object-cover rounded-lg cursor-pointer border ${
+                  mainImage === image.url ? "border-black" : "border-gray-300"
+                }`}
+                onClick={() => setMainImage(image.url)}
               />
             ))}
           </div>
@@ -75,7 +116,12 @@ const ProductDetails = () => {
                 {selectedProduct.colors.map((color) => (
                   <button
                     key={color}
-                    className="w-8 h-8 rounded-full border"
+                    onClick={() => setSelectedColor(color)}
+                    className={`w-8 h-8 rounded-full border ${
+                      selectedColor === color
+                        ? "border-4 border-black"
+                        : "border-gray-300"
+                    }`}
                     style={{
                       backgroundColor: color.toLocaleLowerCase(),
                       filter: "brightness(0.5)",
@@ -89,8 +135,13 @@ const ProductDetails = () => {
               <div className="flex gap-2 mt-2">
                 {selectedProduct.sizes.map((size) => (
                   <button
+                    onClick={() => setSelectedSize(size)}
                     key={size}
-                    className="px-4 py-2 border rounded hover:bg-gray-200"
+                    className={`px-4 py-2  rounded border ${
+                      selectedSize === size
+                        ? "bg-black text-white"
+                        : "border-gray-300"
+                    }`}
                   >
                     {size}
                   </button>
@@ -100,16 +151,16 @@ const ProductDetails = () => {
             <div className="mb-6">
               <p className="text-gray-700">Quantity :</p>
               <div className="flex items-center space-x-4 mt-2">
-                <button className="px-2 py-1 border rounded bg-gray-200">
+                <button onClick={()=> handleQuantityChange("minus")} className="px-2 py-1 border rounded bg-gray-200">
                   -
                 </button>
-                <span className="text-lg">1</span>
-                <button className="px-2 py-1 border rounded bg-gray-200">
+                <span className="text-lg">{quantity}</span>
+                <button onClick={()=> handleQuantityChange("plus")} className="px-2 py-1 border rounded bg-gray-200">
                   +
                 </button>
               </div>
             </div>
-            <button className="w-full bg-black text-white py-2 px-6 rounded-lg font-semibold hover:bg-gray-800 ">
+            <button onClick={handleAddToCart} className="w-full bg-black text-white py-2 px-6 rounded-lg font-semibold hover:bg-gray-800 ">
               Add to Cart
             </button>
             <div className=" mt-10 text-gray-700">
@@ -117,12 +168,12 @@ const ProductDetails = () => {
               <table className="w-full text-left text-sm text-gray-600">
                 <tbody>
                   <tr>
-                    <td className="py-2 ">Brand</td>
-                    <td className="py-2 ">{selectedProduct.brand}</td>
+                    <td className="py-1 ">Brand</td>
+                    <td className="py-1 ">{selectedProduct.brand}</td>
                   </tr>
                   <tr>
-                    <td className="py-2 ">Material</td>
-                    <td className="py-2 ">{selectedProduct.material}</td>
+                    <td className="py-1 ">Material</td>
+                    <td className="py-1 ">{selectedProduct.material}</td>
                   </tr>
                   {/* Add more characteristics as needed */}
                 </tbody>
