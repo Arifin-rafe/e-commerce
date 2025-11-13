f31594a3c1a7ec422cbdf0ffd38fa6128b4ad64f
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2025-11-14 00:45:24 +0600

    filtersidebar: done with it

--- frontend/src/components/Products/FilterSidebar.jsx
+++ frontend/src/components/Products/FilterSidebar.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const FilterSidebar = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
   const [filters, setFilters] = useState({
     category: "",
     gender: "",
@@ -76,16 +77,28 @@ const FilterSidebar = () => {
       newFilters[name] = value;
     }
     setFilters(newFilters);
+    updateURLParams(newFilters);
     console.log(newFilters)
     // setSearchParams(newFilters);
   };
   const updateURLParams = (newFilters) => {
     const params = new URLSearchParams();
     Object.keys(newFilters).forEach((key) => {
-      if(Array.isArray(newFilters[key])){
-        
+      if(Array.isArray(newFilters[key]) && newFilters[key].length > 0){
+        params.append(key, newFilters[key].join(","));
+      } else if(newFilters[key]){
+        params.append(key, newFilters[key]);
       }
-    })
+    });
+    setSearchParams(params);
+    navigate(`?${params.toString()}`);
+  };
+  const handlePriceChange = (e) => {
+    const newPrice = e.target.value;
+    setPriceRange(0,[newPrice]);
+    const newFilters = { ...filters,minPrice: 0, maxPrice: newPrice };
+    setFilters(filters);
+    updateURLParams(newFilters);
   };
   return (
     <div className="p-4">
@@ -102,6 +115,7 @@ const FilterSidebar = () => {
               name="category"
               value={category}
               onChange={handleFilterChange}
+              checked={filters.category === category}
               className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
             />
             <span className="text-gray-700">{category}</span>
@@ -120,7 +134,7 @@ const FilterSidebar = () => {
               name="gender"
               value={gender}
               onChange={handleFilterChange}
-              on
+              checked={filters.gender === gender}
               className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
             />
             <span className="text-gray-700">{gender}</span>
@@ -139,8 +153,9 @@ const FilterSidebar = () => {
               name="color"
               value={color}
               onClick={handleFilterChange}
-              className="w-8 h-8 rounded-full border border-gray-300 
-              cursor-pointer transition hover:scale-105"
+              className={`w-8 h-8 rounded-full border border-gray-300 
+              cursor-pointer transition hover:scale-105
+              ${filters.color === color ? "ring-2 ring-blue-500" : ""}`}
               style={{ backgroundColor: color.toLowerCase() }}
             ></button>
           ))}
@@ -156,6 +171,7 @@ const FilterSidebar = () => {
               name="size"
               value={size}
               onChange={handleFilterChange}
+              checked={filters.size.includes(size)}
               className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
             />
             <span className="text-gray-700">{size}</span>
@@ -172,6 +188,7 @@ const FilterSidebar = () => {
               name="material"
               value={material}
               onChange={handleFilterChange}
+              checked={filters.material.includes(material)}
               className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
             />
             <span className="text-gray-700">{material}</span>
@@ -188,6 +205,7 @@ const FilterSidebar = () => {
               name="brand"
               value={brand}
               onChange={handleFilterChange}
+              checked={filters.brand.includes(brand)}
               className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
             />
             <span className="text-gray-700">{brand}</span>
@@ -203,9 +221,10 @@ const FilterSidebar = () => {
           type="range"
           name="priceRange"
           value={priceRange[1]}
-          onChange={handleFilterChange}
+          onChange={handlePriceChange}
           min={0}
           max={100}
+          
           className="w-full h-2 bg-gray-300 rounded-lg appearance-none cursor-pointer"
         />
         <div className="flex justify-between text-gray-600 mt-2">
