a1a119c8bf50f133439e3c01d280c16607e5fef2
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2025-11-16 23:48:23 +0600

    sortoptions: started working on sort and done basic jsx

--- frontend/src/components/Products/SortOptions.jsx
+++ frontend/src/components/Products/SortOptions.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { useSearchParams } from 'react-router-dom';
 
 const SortOptions = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleSortChange = (e) => {
+    const selectedValue = e.target.value;
+    console.log(selectedValue)
+  }
   return (
-    <div className="">SortOptions</div>
+    <div className="mb-4 flex items-center justify-end">
+      <select id="sort"
+      onChange={handleSortChange}
+      className="border p-2 rounded-md focus:outline-none">
+        <option value="">Default</option>
+        <option value="priceAsc">Price : Low to High</option>
+        <option value="priceDesc">Price : High to Low</option>
+        <option value="popularity">Popularity</option>
+      </select>
+    </div>
   )
 }
 
