8a13270cbd35e07eed4e885d61ee561f0c14a3f8
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2025-11-17 23:22:04 +0600

    checkout page: finish form data

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, } from "react";
+import { useNavigate } from "react-router-dom";
 import { IoMdClose } from "react-icons/io";
 import CartContents from "../Cart/CartContents";
 const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
+const navigate = useNavigate();
+  const handleCheckout = () => {
+    // Handle checkout logic here
+    navigate("/checkout");
+    // toggleCartDrawer();
+  };
   return (
     <div
       className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[30rem] h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${
@@ -22,10 +29,15 @@ const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
       </div>
       {/* checkout button */}
       <div className="p-4 sticky bg-white bottom-0">
-        <button className="w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition">
+        <button
+          onClick={handleCheckout}
+          className="w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition"
+        >
           Check Out
         </button>
-        <p className="text-sm tracking-tighter text-gray-500 mt-2 text-center">Shipping,taxes, and discount codes calculated at checkout.</p>
+        <p className="text-sm tracking-tighter text-gray-500 mt-2 text-center">
+          Shipping,taxes, and discount codes calculated at checkout.
+        </p>
       </div>
     </div>
   );
