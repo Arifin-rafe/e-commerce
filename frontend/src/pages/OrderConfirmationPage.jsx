70641166cd87667e7748f150621eef32d8c80241
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2025-11-21 20:27:29 +0600

    orderConfirmationPage: started work on that

--- frontend/src/pages/OrderConfirmationPage.jsx
+++ frontend/src/pages/OrderConfirmationPage.jsx
@@ -1,5 +1,24 @@
 import React from 'react'
-
+const checkout ={
+  _id:"12323",
+  createdAt: new Date(),
+  checkoutItems: [{
+    productId:"1",
+    name:"Jacket",
+    color:"black",
+    size:"M",
+    price:150,
+    quantity:1,
+    image:"https://picsum.photos/150?random=1"},
+  {
+    productId:"2",
+    name:"T-shirt",
+    color:"black",
+    size:"M",
+    price:120,
+    quantity:2,
+    image:"https://picsum.photos/150?random=1"}],
+}
 const OrderConfirmationPage = () => {
   return (
     <div>OrderConfirmationPage</div>
