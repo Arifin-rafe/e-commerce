d8526aca817bebdc7605a575604b17a442c53527
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2025-11-28 11:25:44 +0600

    orderdetailspage: added order info in frontend

--- frontend/src/pages/OrderDetailsPage.jsx
+++ frontend/src/pages/OrderDetailsPage.jsx
@@ -15,9 +15,9 @@ const OrderDetailsPage = () => {
       shippingMethod: "Standard",
       shippingAddress: {
         city: "New York",
-        country: "USA"
+        country: "USA",
       },
-      orderItems:[
+      orderItems: [
         {
           productId: "1",
           name: "Jacket",
@@ -36,11 +36,29 @@ const OrderDetailsPage = () => {
           quantity: 2,
           image: "https://picsum.photos/150?random=2",
         },
-      ]
+      ],
     };
     setOrderDetails(mockOrderDetails);
   }, [id]);
-  return <div>OrderDetailsPage</div>;
+  return (
+    <div className="max-w-7xl mx-auto p-4 sm:p-6">
+      <h2 className="text-2xl md:text-3xl font-bold mb-6">Order Details</h2>
+      {!orderDetails ? (
+        <p>No order details found</p>
+      ) : (
+        <div className="p-4 sm:p-6 rounded-lg border">
+          {/* order info */}
+          <div className="flex flex-col sm:flex-row justify-between mb-8">
+            <div>
+              <h3 className="text-lg md:text-xl font-semibold">
+                Order ID: #{orderDetails._id}
+              </h3>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default OrderDetailsPage;
