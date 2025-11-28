7b17bb38a15bfa944faf9d182fc0c657ec614cd3
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2025-11-28 17:04:47 +0600

    orderdetailspage: added customer, payment & shipping info

--- frontend/src/pages/OrderDetailsPage.jsx
+++ frontend/src/pages/OrderDetailsPage.jsx
@@ -1,10 +1,10 @@
-import { use, useState } from "react";
+import { use, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 const OrderDetailsPage = () => {
   const { id } = useParams();
   const [orderDetails, setOrderDetails] = useState(null);
 
-  useffect(() => {
+  useEffect(() => {
     //fetch order details by id
     const mockOrderDetails = {
       _id: id,
@@ -46,15 +46,60 @@ const OrderDetailsPage = () => {
       {!orderDetails ? (
         <p>No order details found</p>
       ) : (
-        <div className="p-4 sm:p-6 rounded-lg border">
+        <div className="p-4 sm:p-6 rounded-lg border border-gray-300">
           {/* order info */}
           <div className="flex flex-col sm:flex-row justify-between mb-8">
             <div>
               <h3 className="text-lg md:text-xl font-semibold">
                 Order ID: #{orderDetails._id}
               </h3>
+              <p className="text-gray-600">
+                {new Date(orderDetails.createdAt).toLocaleDateString()}
+              </p>
+            </div>
+            <div className="flex flex-col items-start sm:items-end mt-4 sm: mt-0">
+              <span
+                className={`${
+                  orderDetails.isPaid
+                    ? "bg-green-100 text-green-700"
+                    : "bg-red-100 text-red-700"
+                } px-3 py-1 rounded-full text-sm font-medium mb-2`}
+              >
+                {orderDetails.isPaid ? "Approved" : "Pending"}
+              </span>
+              <span
+                className={`${
+                  orderDetails.isDelivered
+                    ? "bg-green-100 text-green-700"
+                    : "bg-yellow-100 text-yellow-700"
+                } px-3 py-1 rounded-full text-sm font-medium mb-2`}
+              >
+                {orderDetails.isDelivered ? "Delivered" : "Pending Delivery"}
+              </span>
             </div>
           </div>
+          {/* customer, payment & shipping info */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mb-8">
+            <div>
+              <h4 className="text-lg font-semibold mb-2">Payment Info</h4>
+              <p className="text-gray-600">
+                Payment Method: {orderDetails.paymentMethod}
+              </p>
+              <p> Status :{orderDetails.isPaid ? "Paid" : "Unpaid"}</p>
+            </div>
+            <div>
+              <h4 className="text-lg font-semibold mb-2">Shipping Info</h4>
+              <p className="text-gray-600">
+                Shipping Method: {orderDetails.shippingMethod}
+              </p>
+              <p>
+                {" "}
+                Address :{" "}
+                {`${orderDetails.shippingAddress.city}, ${orderDetails.shippingAddress.country}`}
+              </p>
+            </div>
+          </div>
+          {/* Products list */}
         </div>
       )}
     </div>
