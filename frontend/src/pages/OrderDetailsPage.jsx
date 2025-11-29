97c022ddf471e8e12f0f1cf5761e105be590cbad
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2025-11-29 16:35:57 +0600

    orderdetailspage: added Products list of customers

--- frontend/src/pages/OrderDetailsPage.jsx
+++ frontend/src/pages/OrderDetailsPage.jsx
@@ -1,5 +1,5 @@
 import { use, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 const OrderDetailsPage = () => {
   const { id } = useParams();
   const [orderDetails, setOrderDetails] = useState(null);
@@ -100,6 +100,42 @@ const OrderDetailsPage = () => {
             </div>
           </div>
           {/* Products list */}
+          <div className="overflow-x-auto">
+            <h4 className="text-lg font-semibold mb-4">Products</h4>
+            <table className="min-w-full border border-gray-400 mb-4">
+              <thead className="bg-gray-100">
+                <tr>
+                  <th className="px-4 py-2">Name</th>
+                  <th className="px-4 py-2">Unit Price</th>
+                  <th className="px-4 py-2">Quantity</th>
+                  <th className="px-4 py-2">Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                {orderDetails.orderItems.map((item) => (
+                  <tr key={item.productId} className="border-b border-gray-300">
+                    <td className="px-4 py-2 flex items-center">
+                      <img
+                        src={item.image}
+                        alt={item.name}
+                        className="w-12 h-12 object-cover rounded-lg mr-4"
+                      />
+                      <Link
+                        to={`/products/${item.productId}`}
+                        className="text-blue-500 hover:underline"
+                      >
+                        {item.name}
+                      </Link>
+                    </td>
+                    <td className="px-4 py-2">${item.price}</td>
+                    <td className="px-4 py-2">{item.quantity}</td>
+                    <td className="px-4 py-2">${item.price * item.quantity}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          {/* back to orders link */}
         </div>
       )}
     </div>
