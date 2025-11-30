b7f5b741259e641d79bbccbdda6f6b723240163d
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2025-11-30 20:41:51 +0600

    adminlayout: addded mobile toggle button

--- frontend/src/components/Admin/AdminLayout.jsx
+++ frontend/src/components/Admin/AdminLayout.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
 
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
 const AdminLayout = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
@@ -9,6 +10,11 @@ const AdminLayout = () => {
   return (
     <div className="min-h-screen flex flex-col md:flex-row relative">
         {/* Mobile toggle button */}
+        <div className="flex md:hidden p-4 bg-gray-900 text-white z-20">
+            <button onClick={toggleSidebar}>
+                <FaBars size={24} />
+            </button>
+        </div>
     </div>
   )
 }
