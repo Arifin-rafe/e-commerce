bee860efffeedd9a87362c7814ba30e86f36e01f
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2025-12-02 09:54:55 +0600

    adminsidebar: added user products orders shop

--- frontend/src/components/Admin/AdminLayout.jsx
+++ frontend/src/components/Admin/AdminLayout.jsx
@@ -1,22 +1,45 @@
-
 import { useState } from "react";
 import { FaBars } from "react-icons/fa";
+import AdminSidebar from "./AdminSidebar";
+import { Outlet } from "react-router-dom";
 const AdminLayout = () => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-    const toggleSidebar = () => {
-        setIsSidebarOpen(!isSidebarOpen);
-    }
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
   return (
     <div className="min-h-screen flex flex-col md:flex-row relative">
-        {/* Mobile toggle button */}
-        <div className="flex md:hidden p-4 bg-gray-900 text-white z-20">
-            <button onClick={toggleSidebar}>
-                <FaBars size={24} />
-            </button>
-        </div>
+      {/* Mobile toggle button */}
+      <div className="flex md:hidden p-4 bg-gray-900 text-white z-20">
+        <button onClick={toggleSidebar}>
+          <FaBars size={24} />
+        </button>
+      </div>
+      {/* overlay for mobile Sidebar */}
+      {isSidebarOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 z-10 md:hidden"
+          onClick={toggleSidebar}
+        ></div>
+      )}
+      {/* Sidebar */}
+      <div
+        className={`w-64 bg-gray-900 text-white absolute min-h-screen ${
+          isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 md:static md:translate-x-0 md: block z-20`}
+      >
+        {/* Sidebar content */}
+        <AdminSidebar />
+        
+      </div>
+      {/* Main Content */}
+      <div className="flex-grow p-6 overflow-auto">
+        <Outlet></Outlet>
+        {/* Your main content goes here */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminLayout
+export default AdminLayout;
