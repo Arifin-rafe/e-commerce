a19eaa9a8b190794fed06206eac4ae206580ab29
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2025-12-08 13:40:52 +0600

    feat(EditProductPage): implement edit product form and routing

--- frontend/src/components/Admin/EditProductPage.jsx
+++ frontend/src/components/Admin/EditProductPage.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const EditProductPage = () => {
-  return (
-    <div>
-        {/* Implement Edit Product Form Here */}
-    </div>
-  )
-}
+  const [productData, setProductData] = useState({
+    name: "",
+    description: "",
+    price: 0,
+    collections: "",
+    category: "",
+    gender: "",
+    colors: [],
+    sizes: [],
+    material: "",
+    brand: "",
+    countInStock: 0,
+    sku: "",
+    images: [
+      {
+        url: "https://picsum.photos.150?random=1",
+      },
+      {
+        url: "https://picsum.photos.150?random=2",
+      },
+    ],
+  });
 
-export default EditProductPage
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setProductData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  return (
+    <div className="max-w-5xl mx-auto p-6 shadow-md rounded-md">
+      {/* Implement Edit Product Form Here */}
+      <h2 className="text-3xl font-bold mb-6">Edit Product</h2>
+      <form action="">
+        {/* name */}
+        <div className="mb-6">
+          <label className="block mb-2 font-semibold">Product Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={productData.name}
+            onChange={handleChange}
+            className="w-full border border-gray-300 p-2 rounded-md"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default EditProductPage;
