4498d003d2222b79ae474eec46a65552b0326808
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2025-12-14 02:19:31 +0600

    set up mongodb and making schema

---
+++ backend/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const bycrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please add a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      default: "customer",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Password hash middlewire
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-const PORT = 3000
 
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+connectDB();
+
+// Routes
 app.get('/', (req, res) => {
     res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
