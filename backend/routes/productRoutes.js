aa959d34a2bf46bf326e604c47b98db038fbfff0
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2026-02-14 23:42:47 +0600

    refactor product routes to improve structure and readability

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,3 +1,61 @@
-const express = require('express');
+const express = require("express");
+const Product = require("../models/Product");
+const { protect } = require("../middleware/authMiddleware");
+
 const router = express.Router();
-const Product = require('../models/Product');
+
+// @route   POST /api/products
+// @desc    Create a new product
+// @access  Private
+router.post("/", protect, async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      discountPrice,
+      countInStock,
+      category,
+      brand,
+      size,
+      colors,
+      collection,
+      material,
+      gender,
+      images,
+      isFeatured,
+      isPublished,
+      tags,
+      dimensions,
+      weight,
+      sku,
+    } = req.body;
+    const product = new Product({
+      name,
+      description,
+      price,
+      discountPrice,
+      countInStock,
+      category,
+      brand,
+      size,
+      colors,
+      collection,
+      material,
+      gender,
+      images,
+      isFeatured,
+      isPublished,
+      tags,
+      dimensions,
+      weight,
+      sku,
+      user: req.user._id, //reference to the user who created the product
+    });
+    const createdProduct = await product.save();
+    res.status(201).json(createdProduct);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
