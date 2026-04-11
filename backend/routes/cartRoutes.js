6f0f1da20149948d3fe1d2a1e05434b7433c677d
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2026-04-11 23:56:23 +0600

    enhance cartRoutes to support adding products for both guests and logged-in users, including cart creation and total price calculation

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -5,10 +5,75 @@ const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
+// Helper function to get the cart for a guest or logged in user
+const getCart = async (userId, guestId) => {
+        if(userId) {
+            return await Cart.findOne({ user: userId });
+        } else if(guestId) {
+            return await Cart.findOne({ guestId });
+        }
+        return null;
+}
 // @route   POST /api/cart
 // @desc    Add a product to the cart for a guet or logged in user
 // @access  Public
 
 router.post('/', protect, async (req, res) => {
-    
+    const {productId,quantity,size,color,guestId,userId} = req.body;
+    try {
+        const product = await Product.findById(productId);
+        if(!product) return res.status(404).json({ message: "Product not found" });
+
+        // Determine if the user is a guest or logged in
+        let cart = await getCart(guestId, userId);
+
+        // if cart exists, update it
+        if(cart) {
+            const productIndex = cart.products.findIndex((p) => p.productId.toString() === productId && 
+            p.size === size && 
+            p.color === color);
+            if(productIndex > -1) {
+                // if product already exists, update the quantity
+                cart.products[productIndex].quantity += quantity;
+            } else {
+                // if product does not exist, add it to the cart
+                cart.products.push({
+                    productId,
+                    name: product.name,
+                    image: product.images[0].url,
+                    price: product.price,
+                    size,
+                    color,
+                    quantity,
+                });
+            }
+            // recalculate the total price
+            cart.totalPrice = cart.products.reduce((acc, item) => acc + item.price * item.quantity, 
+            0
+        );
+        await cart.save();
+        res.status(200).json(cart);
+        } else {
+            // if cart does not exist, create it
+            const newCart = await Cart.create({
+                userId: userId ? userId : undefined,
+                guestId: guestId ? guestId : "guest_" + new Date().getTime(),
+                products: [{
+                    productId,
+                    name: product.name,
+                    image: product.images[0].url,
+                    price: product.price,
+                    size,
+                    color,
+                    quantity,
+                }],
+                totalPrice: product.price * quantity,
+            });
+            // await cart.save();
+            res.status(201).json(newCart);
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+    }
 })
