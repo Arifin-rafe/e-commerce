d7eb1d81b6825bf426ec988e25ac04f6e3ac4f75
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2026-07-26 06:25:45 +0800

    created order routes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,4 +1,4 @@
-const express = require ("express");
+const express = require("express");
 const Order = require("../models/Order");
 const { protect } = require("../middleware/authMiddleware");
 
@@ -8,4 +8,36 @@ const router = express.Router();
 // @desc Get all orders for the logged-in user
 // @access Private
 
-router.get
+router.get("/my-orders", protect, async (req, res) => {
+  try {
+    // find authenticated user's orders
+    const orders = await Order.find({ user: req.user._id }).sort({
+      createdAt: -1,
+    }); //sort by most recent orders
+    res.json(orders);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// @route GET /api/orders/:id
+// @desc vGet order details by ID
+// @access Private
+router.get("/:id", protect, async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id).populate(
+      "user",
+      "name email",
+    );
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+    res.json(order);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+module.exports = router;
