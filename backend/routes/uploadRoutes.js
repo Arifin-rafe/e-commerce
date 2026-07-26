0cf971af421891dc548eaff772dc47d3b1dcbfda
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2026-07-27 02:54:45 +0800

    add image upload functionality with Cloudinary integration

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,2 +1,21 @@
 const express = require ('express');
 const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+const streamifier = require('streamifier');
+require('dotenv').config();
+const router = express.Router();
+
+// cloudinary config
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+//multer setup using memory storage
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+router.post('/', upload.single('image'), async (req, res) => {
+    
+})
