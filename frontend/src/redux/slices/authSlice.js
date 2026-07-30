0d419d4af765d390a7c86bb55fbdf416ab5d71c6
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2026-07-31 04:10:47 +0800

    feat: add Redux for state management and integrate authentication slice
    - Added Redux Toolkit and React-Redux dependencies to the project.
    - Created authSlice for managing user authentication state, including login and registration async thunks.
    - Implemented guest ID management in local storage.
    - Updated store configuration to prepare for Redux integration.
    - Added backend URL to environment variables for API requests.

---
+++ frontend/src/redux/slices/authSlice.js
@@ -0,0 +1,86 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+//retrive user from local storage if available
+
+const userFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
+// check forr guest ID in the local storage or generate a new one
+
+const initialGuestId =
+  localStorage.getItem("guestId") || `guest_${new Date().getTime()}`;
+localStorage.setItem("guestId", initialGuestId);
+
+// Initial state
+const initialState = {
+  user: userFromStorage,
+  guestId: initialGuestId,
+  loading: false,
+  error: null,
+};
+
+// async thunk for login process
+
+export const loginUser = createAsyncThunk(
+  "auth/loginUser",
+  async (userData, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/api/users/login`,
+        userData,
+      );
+      localStorage.setItem("userInfo", JSON.stringify(response.data.user));
+      localStorage.setItem("userToken", response.data.token);
+
+      return response.data.user;
+    } catch (error) {
+        return rejectWithValue(error.response.data)
+    }
+  },
+);
+// async thunk for registration process
+
+export const registerUser = createAsyncThunk(
+  "auth/registerUser",
+  async (userData, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/api/users/register`,
+        userData,
+      );
+      localStorage.setItem("userInfo", JSON.stringify(response.data.user));
+      localStorage.setItem("userToken", response.data.token);
+
+      return response.data.user;
+    } catch (error) {
+        return rejectWithValue(error.response.data)
+    }
+  },
+);
+// slice
+
+const authSlice = createSlice({
+    name:"auth",
+    initialState,
+    reducers: {
+        logout: (state) =>{
+            state.user = null;
+            state.guestId = `guest_${new Date().getTime()}` // reset guest id on logout
+            localStorage.removeItem("userInfo");
+            localStorage.removeItem("userToken"); 
+            localStorage.setItem("guestId", state.guestId) //set new guest id in local storage
+        },
+        generateNewGuestId: (state) =>{
+            state.guestId = `guest_${new Date().getTime()}`;
+            localStorage.setItem("guestId", state.guestId);
+        },
+    },
+    extraReducers: (builder) =>{
+        builder.addCase(loginUser.pending, (state) =>{
+            state.loading = true;
+            state.error = null;
+        })
+    }
+})
