52cee651ccc888523a9efe079647e4c9e5b47b9c
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2026-08-14 18:36:48 +0800

    implement extra reducers for cart actions and add checkoutSlice

--- frontend/src/redux/slices/cartSlice.js
+++ frontend/src/redux/slices/cartSlice.js
@@ -132,6 +132,77 @@ const cartSlice = createSlice({
     clearCart: (state) => {
       state.cart = { products: [] }
       localStorage.removeItem("cart")
-    }
-  }
-})
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+    .addCase(fetchCart.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+      })
+    .addCase(fetchCart.fulfiled, (state, action) => {
+      state.loading = false;
+      state.cart = action.payload;
+      saveCartToStorage(action.payload);
+      })
+    .addCase(fetchCart.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.error.message || "Failed to fetch cart";
+    })
+    .addCase(addToCart.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+      })
+    .addCase(addToCart.fulfiled, (state, action) => {
+      state.loading = false;
+      state.cart = action.payload;
+      saveCartToStorage(action.payload);
+      })
+    .addCase(addToCart.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload?.message || "Failed to add to cart";
+    })
+    .addCase(updateCartItemQuantity.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+      })
+    .addCase(updateCartItemQuantity.fulfiled, (state, action) => {
+      state.loading = false;
+      state.cart = action.payload;
+      saveCartToStorage(action.payload);
+      })
+    .addCase(updateCartItemQuantity.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload?.message || "Failed to update item quantity";
+    })
+    .addCase(removeFromCart.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+      })
+    .addCase(removeFromCart.fulfiled, (state, action) => {
+      state.loading = false;
+      state.cart = action.payload;
+      saveCartToStorage(action.payload);
+      })
+    .addCase(removeFromCart.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload?.message || "Failed to remove item";
+    })
+    .addCase(mergeCart.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+      })
+    .addCase(mergeCart.fulfiled, (state, action) => {
+      state.loading = false;
+      state.cart = action.payload;
+      saveCartToStorage(action.payload);
+      })
+    .addCase(mergeCart.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload?.message || "Failed to merge cart";
+    });
+  },
+});
+
+export const { clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
